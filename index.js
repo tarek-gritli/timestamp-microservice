e7aca9ea72831ac24a63d57fa34d39b2fcb4a16b
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-04-01 21:43:21 +0100

    Complete project

--- index.js
+++ index.js
@@ -17,15 +17,35 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
+app.get("/api/:date",(req,res)=>{
+  let date = new Date(req.params.date);
+  if(isInvalidDate(date))
+  {
+    date = new Date(+req.params.date)
+  }
+  if (isInvalidDate(date))
+  {
+    res.json({error: "Invalid Date"})
+    return;
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+})
+app.get("/api",(req,res)=>{
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+})
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
